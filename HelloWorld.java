05b42bc9c37711f39907edd5e06cdd27df8e9bc2
Tonia <java.a.ignatjeva@gmail.com>
2017-01-09 18:57:09 +0200

    Scanner

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld
 {
     public static void main (String [] args)
     {
 	System.out.println("Hello World!");
 	System.out.println("Happy New Year!");
+	Scanner sc = new Scanner(System.in);
     }
 }
